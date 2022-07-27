057e43d9390383f19e392313bcbdc3b15bacf901
Fabrício Andry <andrysf28@gmail.com>
2022-07-27 01:48:43 -0300

    m2 exercicios sprint 1 resolvidos

--- src/script.js
+++ src/script.js
@@ -1,13 +1,72 @@
-function aboveAverageGrades() {}
+const grades = [9, 10, 8, 2, 1, 9, 0, 6, 7, 3, 8]
 
-function averageGrades() {}
+// Nota acima da média
+const aboveAverageGrades = (arrGrades => {
+    const media = []
+    for (let i = 0; i < arrGrades.length; i++) {
+        if (arrGrades[i] > 8) {
+          media.push(arrGrades[i])  
+        }
+    }
+    return media
+})
 
-function belowAverageGrades() {}
+// Nota na média
+const averageGrades = (arrGrades => {
+    const media = []
+    for (let i = 0; i < arrGrades.length; i++) {
+        if (arrGrades[i] == 8) {
+            media.push(arrGrades[i])
+        }
+    }
+    return media
+})
 
-function sumOfAllGrades() {}
+// Notas abaixo da média
+const belowAverageGrades = (arrGrades => {
+    const media = []
+    for (let i = 0; i < arrGrades.length; i++) {
+        if (arrGrades[i] < 8) {
+            media.push(arrGrades[i])
+        }
+    }
+    return media
+})
 
-function averageOfGrades() {}
+// Soma total das notas
+const sumOfAllGrades = (arrGrades => {
+    let somaNota = 0
+    for (let i = 0; i < arrGrades.length; i++) {
+        somaNota += arrGrades[i]
+    }
+    return somaNota
+})
 
-function maxGrade() {}
+// A média de todos as notas
+const averageOfGrades = (arrGrades => {
+    let somaNota = 0
+    for (let i = 0; i < arrGrades.length; i++) {
+        somaNota += arrGrades[i]
+    }
+    let media = somaNota / arrGrades.length
 
-function minGrade() {}
+    return media.toFixed(2)
+})
+
+// Retorna a maior nota
+const maxGrade = (arrGrades => {
+    let maiorNota = 0
+    for (let i = 0; i < arrGrades.length; i++) {
+        maiorNota = Math.max(...arrGrades)
+    }
+    return maiorNota
+})
+
+// Retorna a menor nota
+const minGrade = (arrGrades => {
+    let menorNota = 0
+    for (let i = 0; i < arrGrades.length; i++) {
+        menorNota = Math.min(...arrGrades)
+    }
+    return menorNota
+})
